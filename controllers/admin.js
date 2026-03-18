2935e0765eb27c8d2dc17e3d47df0588804562f3
ARY4T <aryathakurx@gmail.com>
2026-03-18 14:55:44 +0530

    Dynamic Routes Added

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,12 +1,11 @@
 const Product = require('../models/product');
 
 exports.getAddProducts = (req, res, next)=>{
-    res.render('admin/add-product', 
+    res.render('admin/edit-product', 
         {pageTitle: 'Add Product', 
         path: '/admin/add-product', 
-        productCSS: true, 
-        formCSS: true, 
-        activeProduct: true});
+        editing: false,
+    });
 }
 
 exports.postAddProduct = (req, res, next)=>{
@@ -15,10 +14,39 @@ exports.postAddProduct = (req, res, next)=>{
     const price = req.body.price;
     const description = req.body.description;
 
-    const product = new Product(title, imageUrl, price, description);
+    const product = new Product(null, title, imageUrl, description, price);
     product.save();
     res.redirect('/'); 
 }
+
+exports.getEditProducts = (req, res, next)=>{
+    const editMode = req.query.edit;
+    if(!editMode){
+        return res.redirect('/');
+    }
+    const prodId = req.params.productId;
+    Product.findById(prodId, product=>{
+        if(!product) return res.redirect('/');
+        res.render('admin/edit-product', 
+        {pageTitle: 'Edit Product', 
+        path: '/admin/edit-product', 
+        editing: editMode,
+        product: product
+        });
+    });
+    
+}
+
+exports.postEditProduct = (req, res, next)=>{
+    const prodId = req.body.productId;
+    const updatedTitle = req.body.title;
+    const updatedPrice = req.body.price;
+    const updatedImageUrl = req.body.imageUrl;
+    const updatedDesc = req.body.description;
+    const updatedProduct = new Product(prodId, updatedTitle, updatedImageUrl, updatedDesc, updatedPrice)
+    updatedProduct.save();
+    res.redirect('/admin/products');
+};
 
 exports.getProducts = (req, res, next)=>{
     Product.fetchAll((products)=>{
@@ -29,4 +57,11 @@ exports.getProducts = (req, res, next)=>{
         }
     )
     }); 
+}
+
+exports.postDeleteProduct = (req, res, next)=>{
+    const prodId = req.body.productId;
+    Product.deleteById(prodId);
+
+    res.redirect('/admin/products');
 }
