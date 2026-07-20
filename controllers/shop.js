6008975ec6d508c2f436b1a5c46dca6c74541791
ARY4T <aryathakurx@gmail.com>
2026-07-20 13:26:13 +0530

    feat: integrate Stripe checkout for payments
    - Add Stripe dependency and checkout session creation
    - Add getCheckout and getCheckoutSuccess controllers
    - Create checkout view with Stripe.js redirect
    - Replace direct order form in cart with checkout flow
    - Add checkout/success and checkout/cancel routes

--- controllers/shop.js
+++ controllers/shop.js
@@ -6,6 +6,10 @@ const PDFDocument = require('pdfkit');
 const Product = require('../models/product');
 const Orders = require('../models/orders');
 
+const keys = require('../keys');
+const product = require('../models/product');
+const stripe = require('stripe')(keys.STRIPE_SECRET_KEY);
+
 const ITEMS_PER_PAGE = 2;
 
 exports.getProducts = (req, res, next) => {
@@ -110,8 +114,55 @@ exports.getCart = (req, res, next) => {
             error.httpStatusCode = 500;
             return next(error);
         });
+}
 
+exports.getCheckout = (req, res, next)=>{
+    let products;
+    let total = 0;
 
+     req.user
+        .populate('cart.items.productId')
+        .then(user => {
+            products = user.cart.items;
+            total = 0;
+            products.forEach(p => {
+                total += p.quantity * p.productId.price;
+            });
+
+            return stripe.checkout.sessions.create({
+                mode: 'payment',
+                payment_method_types: ['card'],
+                line_items: products.map(p => {
+                    return {
+                        price_data: {
+                            currency: 'usd',
+                            unit_amount: Math.round(p.productId.price * 100),
+                            product_data: {
+                                name: p.productId.title,
+                                description: p.productId.description,
+                            },
+                        },
+                        quantity: p.quantity
+                    };
+                }),
+                success_url: req.protocol + '://' + req.get('host') + '/checkout/success',
+                cancel_url: req.protocol + '://' + req.get('host') + '/checkout/cancel'
+            });
+        })
+        .then(session => {
+            res.render('shop/checkout', {
+                path: '/checkout',
+                pageTitle: 'Your Checkout',
+                products: products,
+                totalSum: total,
+                sessionId: session.id
+            });
+        })
+        .catch(err => {
+            const error = new Error(err);
+            error.httpStatusCode = 500;
+            return next(error);
+        });
 }
 
 exports.postCart = (req, res, next) => {
@@ -145,6 +196,39 @@ exports.postCartDeleteProduct = (req, res, next) => {
             return next(error);
         });
 
+};
+
+exports.getCheckoutSuccess = (req, res, next) => {
+
+    req.user
+        .populate('cart.items.productId')
+        .then(user => {
+            const products = user.cart.items.map(i => {
+                return { quantity: i.quantity, product: { ...i.productId._doc } }
+            });
+
+            const order = new Orders({
+                user: {
+                    email: req.user.email,
+                    userId: req.user,
+                },
+                products: products
+
+            });
+            return order.save();
+        })
+        .then((result) => {
+            return req.user.clearCart();
+
+        })
+        .then(() => {
+            res.redirect('/orders');
+        })
+        .catch(err => {
+            const error = new Error(err);
+            error.httpStatusCode = 500;
+            return next(error);
+        });
 };
 
 exports.postOrder = (req, res, next) => {
